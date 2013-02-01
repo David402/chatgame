1f4e99194c34e3845e36221441fbd0416d19b8a9
David Liu <shaowei.liu@gmail.com>
2013-02-01 18:32:44 +0800

    username input field

--- chatgame/AccountViewController.m
+++ chatgame/AccountViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "AccountViewController.h"
+#import "ChatUser.h"
 
-@interface AccountViewController ()
+@interface AccountViewController () <UITextFieldDelegate>
+@property (retain, nonatomic) IBOutlet UITextField *userNameInput;
+- (IBAction)usernameChanged:(id)sender;
 
 @end
 
@@ -28,6 +31,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    self.userNameInput.delegate = self;
+    self.userNameInput.returnKeyType = UIReturnKeyDone;
 }
 
 - (void)didReceiveMemoryWarning
@@ -36,4 +41,40 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dealloc {
+    [_userNameInput release];
+    [super dealloc];
+}
+- (IBAction)usernameChanged:(id)sender {
+    self.userNameInput.enabled = NO;
+    // resign
+    [self.userNameInput resignFirstResponder];
+    
+    ChatUser *user = [[[ChatUser alloc] init] autorelease];
+    user.username = self.userNameInput.text;
+    
+
+#warning stickers from server
+    // get user stickers from server
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField;              // called when 'return' key
+{
+    [self.userNameInput resignFirstResponder];
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.userNameInput.enabled = NO;
+    // resign
+    [self.userNameInput resignFirstResponder];
+    
+    ChatUser *user = [[[ChatUser alloc] init] autorelease];
+    user.username = self.userNameInput.text;
+    
+    
+#warning stickers from server
+    // get user stickers from server
+}
 @end
