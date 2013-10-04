96f7b0784a41af3cae680897db3d1854d57d1815
yyjim <yy.jim731@gmail.com>
2013-10-04 16:44:17 +0800

    added CBUser and CBUserRobot

---
+++ chatgame/CBUser.h
@@ -0,0 +1,19 @@
+//
+//  CBUser.h
+//  chatgame
+//
+//  Created by yyjim on 10/4/13.
+//  Copyright (c) 2013 Cardinal Blue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBUser : NSObject
+@property (nonatomic, copy) NSString *name;
+@end
+
+@interface CBUserRobot : CBUser
+@property (nonatomic, copy) NSArray *phrases;
++ (NSArray *)robots;
+- (NSString *)randomPhrase;
+@end
---
+++ chatgame/CBUser.m
@@ -0,0 +1,36 @@
+//
+//  CBUser.m
+//  chatgame
+//
+//  Created by yyjim on 10/4/13.
+//  Copyright (c) 2013 Cardinal Blue. All rights reserved.
+//
+
+#import "CBToolkit.h"
+#import "CBUser.h"
+
+#define BUNDLE_DIRECTORY_PATH(bundle_name) \
+    [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:bundle_name]
+
+@implementation CBUser
+@end
+
+@implementation CBUserRobot
++ (NSArray *)robots
+{
+    NSArray *array = [NSDictionary dictionaryWithContentsOfFile:BUNDLE_DIRECTORY_PATH(@"robots.plist")];
+    NSArray *robots = [array collect:^id(NSDictionary *info) {
+        CBUserRobot *u = [[[CBUserRobot alloc] init] autorelease];
+        u.name = [info objectForKey:@"name"];
+        u.phrases = [info objectForKey:@"phrases"];
+        return u;
+    }];
+    return robots;
+}
+
+- (NSString *)randomPhrase
+{
+    NSUInteger randomIndex = arc4random() % [self.phrases count];
+    return [self.phrases objectAtIndex:randomIndex];
+}
+@end
